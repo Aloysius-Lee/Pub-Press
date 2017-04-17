8af82f6f5f934af40d06241b212ce52779a1a642
QuanZhuXian <bigshark0810@hotmail.com>
2017-04-17 06:08:03 -0700

    //remove test page

---
+++ PubPress/PubPress/Utils/StripeConstants.swift
@@ -0,0 +1,17 @@
+//
+//  StripeConstants.swift
+//  PubPress
+//
+//  Created by Zhuxian on 4/17/17.
+//  Copyright © 2017 shark. All rights reserved.
+//
+
+import Foundation
+
+
+class StripeConstants {
+    
+    //stripe test account info
+    static let STRIPE_PUBLISH_KEY           = "pk_test_R0Ujo2KZJLU0mIWoFz4MIgYf"
+    static let STRIPE_SECRET_KEY            = "sk_test_5Pjf0HAeANzOue6W9HaPXZ5t"
+}
